29ccdf4e3dc26032ca1feb4f05a6b56b474790d8
narendranath.maddikeri2007@gmail.com <narendranath.maddikeri2007@gmail.com>
2024-08-30 21:31:41 +0530

    initial commit

---
+++ dream-shops/src/test/java/com/backend/dreamshops/DreamShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.dreamshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
